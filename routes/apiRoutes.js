37b63ab68654aacdd27f1dc3a457e7b28bfd9812
Noah A <nanderson815@gmail.com>
2019-03-06 17:10:48 -0500

    added get rout for all prducts

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,18 +2,22 @@ var db = require("../models");
 
 module.exports = function(app) {
   // Get all examples
-  app.get("/api/examples", function(req, res) {
-    
+  app.get("/api/products", function(req, res) {
+    db.Product.findAll({
+      include: [db.Category]
+    }).then(function(dbProduct){
+      res.json(dbProduct);
+    });
   });
 
   // Create a new example
-  app.post("/api/examples", function(req, res) {
+  app.post("/api/products", function(req, res) {
     
   
   });
 
   // Delete an example by id
-  app.delete("/api/examples/:id", function(req, res) {
+  app.delete("/api/products/:id", function(req, res) {
     
   });
 };
