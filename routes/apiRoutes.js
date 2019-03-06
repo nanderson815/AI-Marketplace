7aee1562ddfbba5d9853d2b212c7dd54c29c8c4f
Noah A <nanderson815@gmail.com>
2019-03-06 17:18:49 -0500

    added get, post, delete routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,24 +1,30 @@
 var db = require("../models");
 
-module.exports = function(app) {
+module.exports = function (app) {
   // Get all examples
-  app.get("/api/products", function(req, res) {
+  app.get("/api/products", function (req, res) {
     db.Product.findAll({
       include: [db.Category]
-    }).then(function(dbProduct){
+    }).then(function (dbProduct) {
       res.json(dbProduct);
     });
   });
 
   // Create a new example
-  app.post("/api/products", function(req, res) {
-    db.Product.create(req.body).then(function(dbProduct){
+  app.post("/api/products", function (req, res) {
+    db.Product.create(req.body).then(function (dbProduct) {
       res.json(dbProduct);
     });
   });
 
   // Delete an example by id
-  app.delete("/api/products/:id", function(req, res) {
-    
+  app.delete("/api/products/:id", function (req, res) {
+    db.Product.destroy({
+      where: {
+        id: req.params.id
+      }
+    }).then(function (dbProduct) {
+      res.json(dbProduct);
+    });
   });
 };
