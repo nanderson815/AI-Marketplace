65e8ae7154e00d1ec517357c7301fdd790f578f6
kdaniels <kelsiedaniels@Kelsies-MacBook-Pro-2.local>
2019-03-07 21:01:06 -0500

    Page that creates products for users to upload.

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -11,12 +11,23 @@ module.exports = function (app) {
   });
 
   // Load example page and pass in an example by id
-  app.get("/example/:id", function (req, res) {
-
+  app.get("/product/:id", function (req, res) {
+    db.Product.findOne({where: {id: req.params.id}}).then(function(dpProducts){
+      res.render("editProduct", {
+        product: dpProducts
+      });
+    });
   });
+
+  // Renders Page for adding a new product
+  app.get("/addProduct", function(req, res){
+    res.render("addProduct");
+  });
+
+
 
   // Render 404 page for any unmatched routes
   app.get("*", function (req, res) {
-
+    res.render("404");
   });
 };
