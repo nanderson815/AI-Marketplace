a3ea816e3e725c80476b222e56d446b65aa87b4a
Russell H <russellharben@gmail.com>
2019-03-09 13:53:21 -0500

    Landing Page Display and Return Items by Category

--- public/assets/js/addCategory.js
+++ public/assets/js/addCategory.js
@@ -1,1 +1,25 @@
-alert("Test");
+// Materalize Dropdown
+$('.dropdown-trigger').dropdown();
+
+// // Select items from database based on category button select
+// $("#dropdown1 li").on("click",function(){
+//     console.log($(this).html());
+// })
+
+
+$("#dropdown1 li").on("click",function(){
+    console.log($(this).text());
+    var category = $(this).text();
+
+    var catObj = {
+        category: category
+    }
+
+    $.ajax({
+        method: "GET",
+        url: '/api/categories',
+        data: catObj
+    }).then(function(catResponse){
+        console.log(catResponse);
+    })
+})
