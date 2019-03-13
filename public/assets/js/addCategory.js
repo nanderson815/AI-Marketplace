c42144b1037fd00754005c81da59eeeb3504eaf7
Russell H <russellharben@gmail.com>
2019-03-12 21:51:40 -0400

    category button displays

--- public/assets/js/addCategory.js
+++ public/assets/js/addCategory.js
@@ -6,28 +6,53 @@ $('.dropdown-trigger').dropdown();
 // })
 
 $("#dropdown1 li").on("click",function(){
-    
     var category = $(this).text().toLowerCase();
 
+    // Send Category Name to Query Database
     $.ajax({
         method:"GET",
         url: "categories/" + category
     }).then(function(catResponse){
         console.log(catResponse);
+
         window.location.href = `/addCategories/${category}`;
     });
 
 });
 
-// $(document).on('click', '#dropdown2', function(){
-//     $.ajax({
-//         method:"GET",
-//         url:'categories'
-//     }).then(function(data){
-//         console.log("Categories = ", data);
-//     });
-//     $("#dropdown2").dropdown();
-// });
+$(".popular").on("click",function(){
+    var popCat = $(this).text().toLowerCase();
+
+    // Send Category Name to Query Database
+    $.ajax({
+        method:"GET",
+        url: "categories/" + popCat
+    }).then(function(catResponse){
+        console.log(catResponse);
+
+        window.location.href = `/addCategories/${popCat}`;
+    });
+    
+});
+
+$(window).load(function(){
+    $.ajax({
+        method: "GET",
+        url: 'api/recent'
+    }).then(function(data){
+        console.log(data);
+        var list = $(".categories");
+
+        for(i = 0; i < 5; i++){
+            var item = "<li class='btn btn-small popular'><a href='#'>" + data[i].name + "</a></li>";
+            list.append(item);
+        }
+
+    });
+});
+
+
+
 
 
 
