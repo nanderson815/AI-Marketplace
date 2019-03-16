ac6c48a6c6eb3288579c0c5955b2bce9708f8679
Rich Jen <rich.rjen@gmail.com>
2019-03-16 02:22:58 -0400

    added favicon added icon for home fixed footer fixed bug with categories going to undefined path fixed color scheme issues with buttons and drop down menus across the site

--- public/assets/js/addCategory.js
+++ public/assets/js/addCategory.js
@@ -1,4 +1,3 @@
-
 function clearFilter(){
     $.ajax({
         method: "GET",
@@ -23,9 +22,7 @@ function catButtons() {
 };
 
 function selectCategory() {
-    // Set Variable Equal to Category
     let category = $(this).data('category');
-    // Send Category Name to Query Database
     $.ajax({
         method: "GET",
         url: "addCategories/" + category
@@ -34,10 +31,53 @@ function selectCategory() {
     });
 };
 
+function checkForCategory(path) {
+    console.log(path);
+    switch(path) {
+        case '/addCategories/Sports':
+          $('.popular').attr('data-category', 'Sports');
+          break;
+        
+          case '/addCategories/Cars':
+          $('.popular').attr('data-category', 'Cars');
+          break;
+
+          case '/addCategories/Leisure':
+          $('.popular').attr('data-category', 'Leisure');
+          break;
+
+          case '/addCategories/Fashion':
+          $('.popular').attr('data-category', 'Fashion');
+          break;
+
+          case '/addCategories/Education':
+          $('.popular').attr('data-category', 'Education');
+          break;
+
+          case '/addCategories/Lifestyle':
+          $('.popular').attr('data-category', 'Lifestyle');
+          break;
+
+          case '/addCategories/Home%20Goods':
+          $('.popular').attr('data-category', 'Home Goods');
+          break;
+
+          case '/addCategories/Technology':
+          $('.popular').attr('data-category', 'Technology');
+          break;
+
+        default:
+          console.log('Url unrecognized');
+      }
+}
+
+
+checkForCategory(window.location.pathname);
+
 $(".dropdown-trigger").dropdown();
 $("#dropdown1 li").on("click", selectCategory);
+
 $(document).on("click", ".popular", selectCategory);
 $(document).on("click", "#clearBtn", clearFilter);
-
 
 catButtons();
